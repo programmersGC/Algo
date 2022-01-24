efbefa7a75813832c9a6f6f0b75b00c4096092ca
kgh <kgh01509@naver.com>
2022-01-25 08:33:28 +0900

    algo

---
+++ 기호/Solution_pro_부족한금액계산하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+        long total=0;
+        for(int i = 1; i <= count; i++){
+            total += price * i;
+        }
+        answer = total - money;
+        if(answer < 0) answer =0;
+        return answer;
+    }
+}
