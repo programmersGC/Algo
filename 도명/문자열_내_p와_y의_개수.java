206f4115035f9a9bf3208852238b2f2532a372b5
dobroad <98mengdo.gmail.com>
2022-01-26 17:12:43 +0900

    0126

---
+++ 도명/문자열_내_p와_y의_개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    boolean solution(String s) {
+		boolean answer = true;
+		int cnt = 0;
+		char ch = ' ';
+		
+		for (int i = 0; i < s.length(); i++) {
+			ch = s.charAt(i);
+			if(ch == 'p' || ch== 'P')
+				cnt++;
+			else if (ch == 'y' || ch == 'Y')
+				cnt--;
+		}
+		if(cnt != 0)
+			return false;
+		return true;
+    }
+}
