5bede85b2d593068e285d385ccc662de0ceb8a5e
onehousesilver <onehousesilver@naver.com>
2022-02-08 02:02:56 +0900

    2022-02-07

--- 채은/Level 2/구명보트.py
+++ 채은/Level 2/구명보트.py
@@ -1,2 +1,19 @@
-# solving
-# 02-06 푸는중 ~ 0207까지 풀기
+
+def solution(people, limit):
+
+    answer = 0
+
+    people.sort()
+
+    start_index = 0
+    end_index = len(people) - 1
+
+    while start_index <= end_index:
+        if people[start_index] + people[end_index] <= limit:
+            start_index += 1
+        end_index -= 1
+        answer += 1
+
+    return print(answer)
+
+solution(people = [70, 50, 80, 50], limit = 100)
